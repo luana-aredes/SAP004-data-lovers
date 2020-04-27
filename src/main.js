b3532a16248d90f08acf4b1b98f86d411c0e477f
Karine Sardela <kfsardela@gmail.com>
2020-04-27 19:58:47 -0300

    criando cards e exibindo-os na pagina inicial

--- src/main.js
+++ src/main.js
@@ -3,4 +3,20 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, pokemons);
+let cards = ""
+
+const pokemons = data.pokemon
+
+for (const pokemon of pokemons) {
+    cards += '<div class="card">'
+    cards += '  <div><img src="' + pokemon.img + '" alt="' + pokemon.name + '"></div>'
+    cards += '  <div class="numero">' + pokemon.num + '</div>'
+    cards += '  <div class="nome">' + pokemon.name + '</div>'
+    cards += '  <div class="tipo">Tipo:' + pokemon.type + '</div>'
+    cards += '  <div class="ovos">Ovos:' + pokemon.egg + '</div>'
+    cards += '</div>'
+}
+
+document.getElementById("pokemonList").innerHTML = cards
+
