71d3bb006bc9dcb550722a411cb7cd2beca71364
Karine Sardela <kfsardela@gmail.com>
2020-05-06 14:06:14 -0300

    Finalizada a função de ordenar

--- src/data.js
+++ src/data.js
@@ -5,13 +5,25 @@ computeStats(data): essa função nos permite fazer cálculos estatísticos bás
  os dados permitem.*/
 
 export const sortData = (data, sortBy, sortOrder) => {
- /*data[].sort()
- if === desc  .reverse()*/
-  return data;
-};
+  data.sort(function (a, b) {
+    if (a[sortBy] > b[sortBy]) {
+      return 1;
+    }
+    if (a[sortBy] < b[sortBy]) {
+      return -1;
+    }
+    // a must be equal to b
+    return 0;
+  });
+  
+  if (sortOrder === "desc") {
+    data.reverse()
+  }
 
-/*sortData(data, sortBy, sortOrder): esta função recebe três parâmetros. O primeiro, data, nos entrega os dados. 
-O segundo, sortBy, diz respeito a qual das informações quer usar para ordenar. O terceiro, sortOrder, indica se 
+  return data
+}
+/*sortData(data, sortBy, sortOrder): esta função recebe três parâmetros. O primeiro, data, nos entrega os dados.
+O segundo, sortBy, diz respeito a qual das informações quer usar para ordenar. O terceiro, sortOrder, indica se
 quer ordenar de maneira crescente ou decrescente.*/
 
 
