0982ef4806ef7eb79227472264049d353d756148
Luana Arêdes Costa <luanaredes@hotmail.com>
2020-09-03 11:31:14 -0300

    Resolvendo bug nas rotas

--- src/index.js
+++ src/index.js
@@ -14,13 +14,13 @@ const rout = async () => {
     <nav class="nav" id="nav">
       <ul>
         <li class="navigation-item">
-          <a href="/#"> Página Inicial </a>
+          <a href="./#home"> Página Inicial </a>
         </li>
         <li class="navigation-item">
-          <a href="/#tips"> Dicas do Jogo </a>
+          <a href="./#tips"> Dicas do Jogo </a>
         </li>
         <li class="navigation-item">
-          <a href="/#calculator"> Calculadora </a>
+          <a href="./#calculator"> Calculadora </a>
         </li>
       </ul>
     </nav>
@@ -37,7 +37,7 @@ const rout = async () => {
 
 
   switch (window.location.hash) {
-    case " ":
+    case "#home":
       main.appendChild(await home());
       break;
     case "#calculator":
